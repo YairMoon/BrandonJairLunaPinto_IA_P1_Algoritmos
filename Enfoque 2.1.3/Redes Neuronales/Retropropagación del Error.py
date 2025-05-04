9d1d37980624000ff075009866aaf1c5a87b1ccb
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-05-04 17:52:09 -0600

    se modifica sintaxis

--- Enfoque 2.1.3/Redes Neuronales/Retropropagación del Error.py
+++ Enfoque 2.1.3/Redes Neuronales/Retropropagación del Error.py
@@ -20,7 +20,7 @@ class RedNeuronal:
         self.bias2 = np.random.rand(1)
 
     def entrenar(self):
-        for _ in range(self.epocas):
+        for epoch in range(self.epocas):
             # Propagación hacia adelante
             capa_oculta = sigmoid(np.dot(self.entradas, self.pesos1) + self.bias1)
             salida_pred = sigmoid(np.dot(capa_oculta, self.pesos2) + self.bias2)
@@ -35,4 +35,24 @@ class RedNeuronal:
             # Ajuste de pesos
             self.pesos2 += capa_oculta.T.dot(d_error) * self.tasa_aprendizaje
             self.pesos1 += self.entradas.T.dot(d_capa_oculta) * self.tasa_aprendizaje
-            self.bias2 += np
+            self.bias2 += np.sum(d_error, axis=0) * self.tasa_aprendizaje
+            self.bias1 += np.sum(d_capa_oculta, axis=0) * self.tasa_aprendizaje
+
+    def predecir(self, entradas):
+        # Propagación hacia adelante para hacer predicciones
+        capa_oculta = sigmoid(np.dot(entradas, self.pesos1) + self.bias1)
+        salida_pred = sigmoid(np.dot(capa_oculta, self.pesos2) + self.bias2)
+        return salida_pred
+
+# Datos de entrenamiento (función XOR)
+entradas = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+salidas = np.array([[0], [1], [1], [0]])
+
+# Crear e entrenar la red neuronal
+red_neuronal = RedNeuronal(entradas, salidas)
+red_neuronal.entrenar()
+
+# Predicciones después de entrenamiento
+predicciones = red_neuronal.predecir(entradas)
+print("Predicciones después de entrenamiento:")
+print(predicciones)
