5668ef3a8bc9ed108d8bb2eb6c648cc2febcaab7
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-05-04 21:33:12 -0600

    nuevo algoritmo de eventos

---
+++ Enfoque 2.1.4/Representar el Conocimiento/Eventos y Objetos Mentales Creencias.py
@@ -0,0 +1,8 @@
+# Modelo simple de creencias usando diccionarios
+creencias = {
+    "Juan": ["La tierra es redonda", "El clima está cambiando"],
+    "Ana": ["Los gatos son mágicos", "La tierra es plana"]
+}
+
+# Mostramos las creencias de Ana
+print("Creencias de Ana:", creencias["Ana"])
